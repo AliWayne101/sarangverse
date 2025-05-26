fe7d6c74c9fe4d017bcae11a4a3df69b3e34d403
AliWayne101 <alimalikwayne@gmail.com>
2025-05-27 00:22:35 +0500

    Next-Auth

---
+++ Providers.tsx
@@ -0,0 +1,10 @@
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+export default function Providers({ session, children }: { session: Session | null, children: React.ReactNode}) {
+    return (
+        <SessionProvider session={session}>
+            {children}
+        </SessionProvider>
+    )
+}
